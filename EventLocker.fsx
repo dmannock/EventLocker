8fbde64482abe30e31bd243e2469d1d78426103d
Dan Mannock <dmannock@users.noreply.github.com>
2020-01-25 18:30:01 +0000

    fix #3 dependency resolution for nuget packages, loading of types for markerType search and subtypes

--- EventLocker.fsx
+++ EventLocker.fsx
@@ -1,15 +1,61 @@
 open System
+open System.Reflection
+open System.IO
+open System.Text.RegularExpressions
+
 [<Literal>]
 let EventLockFileName = "lockedevents.hash"
 
-open System.Reflection
+let getPackageFoldersFromPropsFile path =
+    let data = File.ReadAllText path
+    let regexMatch = Regex.Match(data, "<NuGetPackageFolders.*?>(.*?)</NuGetPackageFolders>")
+    if regexMatch.Success then regexMatch.Groups.[1].Value.Split(';') |> Some
+    else None
+
 let tryLoadAssembly a =
     try
         a |> Assembly.LoadFile |> Some
     with
-        | _ -> None    
+        | _ -> 
+            None    
 
-open System.IO
+let genPossibleAssemblyPaths (mainAssemblyPath: string) projectPath nugetPackagePaths dependencyName version frameworkVer =
+    seq {
+        yield Path.GetDirectoryName(mainAssemblyPath)
+        yield Path.Combine(projectPath, "packages", "lib")
+        yield! nugetPackagePaths 
+            |> List.map (fun nugetPath -> Path.Combine(nugetPath, dependencyName, version, "lib", frameworkVer))
+        yield! nugetPackagePaths
+            |> List.choose (fun nugetPath ->
+                Path.Combine(nugetPath, dependencyName)
+                |> Some
+                |> Option.filter Directory.Exists
+                |> Option.bind (Directory.GetDirectories >> Array.sort >> Array.tryLast)
+            )
+            |> List.map (fun latestPath -> Path.Combine(latestPath, "lib", frameworkVer))
+    }
+
+let tryLoadAssemblyForPaths genPossiblePathsForDependency (assemblyFullNameOrPath: String) =
+    match assemblyFullNameOrPath.Split(',') with
+    | [|dependencyName;_;_;_|] when AppDomain.CurrentDomain.GetAssemblies() |> Array.exists (fun a -> a.FullName = dependencyName) ->
+        None
+    // quick filter to save loading asseblies we know we dont need
+    | [|dependencyName;_;_;_|] when dependencyName.StartsWith("System.") || dependencyName.StartsWith("Microsoft.") || dependencyName.StartsWith("FSharp.Core") ->
+        None
+    | [|dependencyName;rawVersion;_;_|] ->
+        let version = rawVersion.Replace(" Version=", "")
+        let filename = dependencyName + ".dll"
+        //TODO: use cases that force this shortcut to be resolved properly
+        let frameworkVer = "netstandard2.0"
+        let possiblePaths = genPossiblePathsForDependency dependencyName version frameworkVer
+        possiblePaths
+        |> Seq.distinct
+        |> Seq.map (fun p -> Path.Combine(p, filename))
+        |> Seq.tryFind File.Exists
+        |> Option.bind tryLoadAssembly
+    | _ -> 
+        tryLoadAssembly assemblyFullNameOrPath 
+
 let getAssemblyDirectory (a: Assembly) =
     let uri = UriBuilder(a.CodeBase)
     let path = Uri.UnescapeDataString uri.Path
@@ -17,18 +63,16 @@ let getAssemblyDirectory (a: Assembly) =
 
 let loadAssemblyWithDependencyResolution mainAssemblyPath =
     let currentDomainAssemblyResolve = new ResolveEventHandler(fun _ args ->
-        let currentAssemblies = AppDomain.CurrentDomain.GetAssemblies()
-        match currentAssemblies |> Array.tryFind (fun a -> a.FullName = args.Name) with
-        | Some(resolvedAssembly) -> resolvedAssembly
-        | None ->
-            let assemblyDir = args.RequestingAssembly |> getAssemblyDirectory
-            let filename = (args.Name.Split(',').[0]) + ".dll".ToLower()
-            Path.Combine(assemblyDir, filename)
-            |> tryLoadAssembly
-            |> Option.defaultValue null
-    )
+        if args.Name.StartsWith("System.") then null
+        else
+            match AppDomain.CurrentDomain.GetAssemblies() |> Array.tryFind (fun a -> a.FullName = args.Name) with
+            | Some(resolvedAssembly) -> 
+                resolvedAssembly
+            | None ->
+                null
+        )
     AppDomain.CurrentDomain.add_AssemblyResolve currentDomainAssemblyResolve
-    mainAssemblyPath |> Assembly.LoadFile
+    mainAssemblyPath |> tryLoadAssembly
 
 let getAllTypes (asm: Assembly) =
     let filter = Array.filter (fun (t: Type) -> (isNull t |> not) && t.Assembly = asm)
@@ -37,22 +81,7 @@ let getAllTypes (asm: Assembly) =
     with
         | :? ReflectionTypeLoadException as ex -> ex.Types |> filter
 
-let getTypesSubclassing (markerType: Type) = 
-    List.filter (fun t -> markerType <> t && not t.IsAbstract && (markerType.IsAssignableFrom t || t.IsSubclassOf markerType))
-
-let getChildTypes markerType types =
-    let rec loop parent remaining found =
-        match remaining with
-        | x::xs -> loop x xs found@(getTypesSubclassing parent remaining)
-        | _ -> []    
-    loop markerType types []
-
-let getMarkerType (marker: string) allTypes = allTypes |> List.tryFind (fun (t: Type) -> t.FullName.EndsWith(marker))
-
-let getMarkerTypeFromAssembly markerType assemblyTypes = 
-    match getMarkerType markerType assemblyTypes with
-    | Some(t) -> t
-    | None -> failwithf "Unable to find marker type '%s'" markerType
+let getMarkerType (markerTypeName: string) allTypes = allTypes |> List.tryFind (fun (t: Type) -> t.FullName.EndsWith(markerTypeName))
 
 open Microsoft.FSharp.Reflection
 // Taken from https://github.com/dmannock/FSharpUnionHelpers
@@ -91,8 +120,7 @@ let rec getTypesPublicSignature (t: Type) =
         let unions = 
             getUnionCases t
             |> Seq.sortBy (fun uc -> uc.Name)
-            |> Seq.map (ucInfo)
-            |> Seq.collect id
+            |> Seq.collect ucInfo
             |> List.ofSeq
         UnionTypeSig(t.Name, unions) 
     else if t.IsClass && t <> typeof<String> && not (typeof<Collections.IEnumerable>.IsAssignableFrom(t)) then
@@ -120,12 +148,17 @@ let rec toSignatureString signature =
 let groupUnionCaseEvents =
     function
     // filter out Unions where the union type & case type implement the marker type
-    | UnionTypeSig(typeName, unions) as evt -> 
+    | UnionTypeSig(typeName, unions) -> 
         match unions |> List.tryFind (fun u -> u.Identifier = typeName) with
         | Some(_) -> List.empty
         | None -> 
             unions |> List.map (fun uc -> Some(uc.Identifier), UnionTypeSig(uc.Identifier, List.singleton uc))
     | evt -> List.singleton (None, evt)
+
+let isConcreteSubtypeOf (markerType: Type) (t: Type) = 
+    if isUnion t 
+    then markerType <> t && (markerType.IsAssignableFrom t || t.IsSubclassOf markerType)
+    else markerType <> t && t.IsPublic && not t.IsAbstract && (markerType.IsAssignableFrom t || t.IsSubclassOf markerType)
 
 type EventHash =  {
     Type: string 
@@ -142,21 +175,42 @@ let typeToEventHash (hashFn: string -> string) (t: Type) =
         |> hashFn
         |> createEventHash (fst x |> Option.defaultValue t.Name))
 
-let getEventHashesForAssembly markerType hashFn assemblyPath =
+let getEventHashesForAssembly markerTypeName hashFn assemblyPath projectPath =
+    let nugetPackagePaths = 
+        Path.Combine(projectPath, "obj")
+        |> Directory.GetFiles
+        |> Seq.tryFind(fun f -> f.EndsWith("nuget.g.props"))
+        |> Option.bind getPackageFoldersFromPropsFile
+        |> Option.map List.ofArray 
+        |> Option.defaultWith (fun () -> 
+            printfn "failed to get nuget package paths. attempting to continue without resolving those packages"
+            List.empty
+        )
     let mainAssembly = assemblyPath |> loadAssemblyWithDependencyResolution
-    let assemblyDir = mainAssembly |> getAssemblyDirectory
-    let loadedDeps = 
-        mainAssembly.GetReferencedAssemblies()
-        |> Array.map (fun aName -> Path.Combine(assemblyDir, aName.Name))
-        |> Array.choose tryLoadAssembly
-        |> Array.distinct
-        |> List.ofArray
-    let assemblyTypes = 
-        mainAssembly::loadedDeps
-        |> List.collect (getAllTypes >> List.ofArray) 
-    let markerType = getMarkerTypeFromAssembly markerType assemblyTypes
-    getChildTypes markerType assemblyTypes
-    |> List.collect (typeToEventHash hashFn)
+    let genPossiblePathsForDependency = genPossibleAssemblyPaths assemblyPath projectPath nugetPackagePaths
+    match mainAssembly with
+    | Some(loaded) -> 
+        let refFullPaths = 
+            loaded.GetReferencedAssemblies()
+            |> Array.map (fun aName -> aName.FullName)
+        let loadedDeps = 
+            refFullPaths
+            |> Array.choose (tryLoadAssemblyForPaths genPossiblePathsForDependency)
+            |> Array.distinct
+            |> List.ofArray
+        let assemblyTypes = 
+            loaded::loadedDeps
+            |> List.collect (getAllTypes >> List.ofArray) 
+        let markerType = 
+            match getMarkerType markerTypeName assemblyTypes with
+            | Some(t) -> t
+            | None -> failwithf "Unable to find marker type '%s'" markerTypeName
+        let childTypes = 
+            assemblyTypes
+            |> List.filter (isConcreteSubtypeOf markerType)
+        childTypes
+        |> List.collect (typeToEventHash hashFn)
+    | None -> failwith "failed loading main assembly"
 
 //event hash lock file comparison
 type EventComparison =
@@ -198,7 +252,7 @@ let readEventHashesFromFile file =
 
 let saveEventHashesToFile file eventHashes =
     let lines = eventHashes |> Seq.map (fun { Type = t; Hash = hash} -> sprintf "%s,%s" t hash)
-    IO.File.WriteAllLines(file, lines)
+    File.WriteAllLines(file, lines)
 
 let hashSha (text: string) =
     if String.IsNullOrEmpty text then String.Empty
@@ -206,10 +260,10 @@ let hashSha (text: string) =
         use sha = new Security.Cryptography.SHA256Managed()
         (System.Text.Encoding.UTF8.GetBytes text |> sha.ComputeHash |> BitConverter.ToString).Replace("-", String.Empty)
 
-let runBuildHashComparison markerType assemblyPath hashLockFileLocation =
-    match IO.Path.Combine(hashLockFileLocation, EventLockFileName) |> readEventHashesFromFile with
+let runBuildHashComparison markerType assemblyPath projectPath =
+    match Path.Combine(projectPath, EventLockFileName) |> readEventHashesFromFile with
     | Some(originalEventHashes) -> 
-        getEventHashesForAssembly markerType hashSha assemblyPath
+        getEventHashesForAssembly markerType hashSha assemblyPath projectPath
         |> compareEventHash originalEventHashes
         |> List.choose (
             function 
@@ -223,14 +277,15 @@ let runBuildHashComparison markerType assemblyPath hashLockFileLocation =
         | errors -> Error (sprintf "Errors in event checks:\n%s" (String.Join("\n", errors)))
     | None -> 
         Error (sprintf "No event lock file found. To start using event locking generate the initial lock by runing with the '--addnew' argument:\n\
-            dotnet fsi EventLocker.fsx \"%s\" \"%s\" --addnew\n" assemblyPath hashLockFileLocation)
+            dotnet fsi EventLocker.fsx \"%s\" \"%s\" --addnew\n" assemblyPath projectPath)
 
-let runAddNewHashes markerType assemblyPath hashLockFilePath =
+let runAddNewHashes markerType assemblyPath projectPath =
+    let hashLockFilePath = Path.Combine(projectPath, EventLockFileName)
     let originalEventHashes = 
         readEventHashesFromFile hashLockFilePath
         |> Option.defaultValue List.Empty
     let eventComparisons = 
-        getEventHashesForAssembly markerType hashSha assemblyPath
+        getEventHashesForAssembly markerType hashSha assemblyPath projectPath
         |> compareEventHash originalEventHashes
     let mutatedEvents = eventComparisons |> List.choose (function 
             | EventSignatureChanged(orig, changed) -> Some (sprintf "Changed Event '%s'. Original hash: %s Current hash: %s" orig.Type orig.Hash changed.Hash)
@@ -289,7 +344,7 @@ let run commandLineOptions =
     printfn "\n##########################################################################\n"
     printfn "Running event locker with options:\n %A" commandLineOptions
     match commandLineOptions with
-    | { RunMode = ForceEventUpdates } as cmd -> runAddNewHashes cmd.MarkerType cmd.AssemblyPath (IO.Path.Combine(cmd.HashLockFileLocation, EventLockFileName))
+    | { RunMode = ForceEventUpdates } as cmd -> runAddNewHashes cmd.MarkerType cmd.AssemblyPath cmd.HashLockFileLocation
     | { RunMode = CompareEvents } as cmd-> runBuildHashComparison cmd.MarkerType cmd.AssemblyPath cmd.HashLockFileLocation
 
 let printResult res = 
